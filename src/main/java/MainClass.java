5ed1b773f1ae71bbe57a8a73d6c2a013e5921fec
kloginov <kvlogino@mts.ru>
2018-12-03 19:28:48 +0300

    Задание 2

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,6 +2,11 @@ import org.junit.Test;
 
 public class MainClass {
 
+private Integer class_number = 20;
+
+public Integer getClassNumber(){
+    return class_number;
+}
 
     public Integer getLocalNumber() {
         return 14;
--- src/main/java/MainClassTest.java
+++ src/main/java/MainClassTest.java
@@ -1,16 +1,26 @@
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.*;
 import static org.junit.Assert.assertEquals;
-
+import static org.hamcrest.BaseMatcher.*;
+import static org.junit.Assert.assertTrue;
 
 public class MainClassTest {
 
 
-    @Test
+    @Test @Ignore
     public void testGetLocalNumber(){
         MainClass el = new MainClass();
         Integer result = el.getLocalNumber();
         assertEquals("Результат вызова функции отличен от 14",result,(Integer) 14);
+    }
+
+
+    @Test
+    public void testGetCLassNumber(){
+        MainClass el = new MainClass();
+        assertTrue("Значение "+ el.getClassNumber() + " меньше, чем 45." ,el.getClassNumber()>45);
     }
 }
